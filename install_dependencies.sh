c78b19b005df931c45cf8209e788914e7ed4d98b
Alicia Garrido Peña <alicia.garrido@uam.es>
2021-09-24 12:18:34 +0100

    New script to install dependencies for python3. (Alternative to conda enviroment)

---
+++ install_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+pip3 install sklearn
+pip3 install pandas
+pip3 install scipy
+pip3 install dill
+pip3 install tableprint
+pip3 install matplotlib
+pip3 install seaborn
+pip3 install elephant
+pip3 install neo
+# If neo does not work, install from source:
+# git clone git://github.com/NeuralEnsemble/python-neo.git
+# cd python-neo
+# python setup.py install
+
